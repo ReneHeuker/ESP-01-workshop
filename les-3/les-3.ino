ea1ced4dde7c22e8681aace6ec4a5ca69a468f4b
ReneHeuker <45095597+ReneHeuker@users.noreply.github.com>
2025-02-23 15:33:44 +0100

    Update les-3.ino

--- les-3/les-3.ino
+++ les-3/les-3.ino
@@ -6,8 +6,8 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-const char* ssid = "CaReSoft";
-const char* password = "2905196902";
+const char* ssid = "Gino WIFI Gasten";
+const char* password = "GinoGasten!";
 String hostname = "workshop-<<yourname>>";
 
 ESP8266WebServer server(80);
